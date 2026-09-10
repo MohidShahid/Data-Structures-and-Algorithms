3e2af98ab3b0a36553380766b494fb1498ed1891
Mohid Shahid <mohid.fantasy@gmail.com>
2026-09-10 10:36:50 +0500

    recurison basic questions practice on how it works and how to define its recurrence relation and recursion tree

--- Recursion/index.js
+++ Recursion/index.js
@@ -1,1 +1,40 @@
 // Sum of N Numbers in Recurison
+
+// Linear Recursion one function does only a single call
+function Sum(n){
+    if (n == 0 ){
+        return 0; // base case
+    }
+
+   return Sum(n-1) + n;  // recursive case
+}
+
+let result
+// result = Sum(5);
+
+// console.log(result);
+
+function Power (x , n){
+    if (n == 1 ){
+        return x;
+    }
+
+   return Power(x, n-1) * x;
+}
+
+
+// result = Power(5, 5);
+
+
+function factorial(n){
+    if(n == 0 || n == 1){
+        return 1;
+    }
+    return factorial(n-1) * n;
+}
+result = factorial(5)
+console.log(result);
+
+
+
+
